3992eb1af090e3138b7b45add0d96d7d9d24bebc
brito101 <rodrigobrito101@hotmail.com>
2023-02-23 21:54:27 -0300

    Change service orders view

--- database/migrations/2023_02_23_205634_alter_view_service_orders_add_address_client.php
+++ database/migrations/2023_02_23_205634_alter_view_service_orders_add_address_client.php
@@ -16,7 +16,7 @@ class AlterViewServiceOrdersAddAddressClient extends Migration
     {
         DB::statement("
         CREATE OR REPLACE VIEW `service_orders_view` AS
-        SELECT so.id, ac.name as activity, cl.name as client, so.user_id, us.name as collaborator, so.priority, so.deadline, so.status, author as author_id, aut.name as author, so.readiness_date, so.number_series, sub.alias_name as subsidiary, so.subsidiary_id, CONCAT(cl.street, ', nº ', cl.number, ', ', cl.neighborhood, '. CEP: ', cl.zipcode, '. ', cl.city, '-', cl.state) as address
+        SELECT so.id, ac.name as activity, cl.name as client, so.user_id, us.name as collaborator, so.priority, so.deadline, so.status, author as author_id, aut.name as author, so.readiness_date, so.number_series, sub.alias_name as subsidiary, so.subsidiary_id, CONCAT(so.street, ', nº ', so.number, ', ', so.neighborhood, '. CEP: ', so.zipcode, '. ', so.city, '-', so.state) as address
         FROM service_orders as so
         LEFT JOIN activities ac ON ac.id = so.activity_id
         LEFT JOIN clients cl ON cl.id = so.client_id
